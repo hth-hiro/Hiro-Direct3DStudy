6fd0a15508612a0ded5957169c8af4720fd04259
hth-hiro <hth0114@naver.com>
2025-09-15 12:25:20 +0900

    fix : PixelShader 수정, 라이팅 효과 적용

--- 03_Light/BasicPixelShader.hlsl
+++ 03_Light/BasicPixelShader.hlsl
@@ -5,14 +5,20 @@
 //--------------------------------------------------------------------------------------
 float4 main(PS_INPUT input) : SV_Target
 {
-    float4 finalColor = {1.0,1.0,0.0,0.0};
+    float4 finalColor = { 0, 0, 0, 0 };
     
-    //float3 N = normalize(input.Norm);
-    //float3 L = normalize(vLightDir.xyz);
-    //finalColor.rgb += saturate(dot(N, L)) * vLightColor.xyz;
+    float4 surfaceColor = float4(0, 1, 0, 0);
     
-    finalColor += saturate(dot((float3) vLightDir, input.Norm) * vLightColor);
+    float3 N = normalize(input.Norm);       // 노멀 벡터
+    float3 L = normalize(vLightDir.xyz);    // 빛의 방향
     
-    finalColor.a = 1;
+    float diffuse = saturate(dot(N, L));    // 입사량
+    
+    // 최종 RGB : 빛의 색상 * 표면의 색상 * 입사량 ( N · L )
+    // 벡터와 스칼라 곱의 연산이므로 순서가 중요하지 않음.
+    finalColor.rgb = vLightColor.rgb * surfaceColor.rgb * diffuse;
+
+    // 최종 Alpha : 텍스처의 Alpha 사용 -> 투명 재질 처리용
+    finalColor.a = surfaceColor.a;
     return finalColor;
 }
