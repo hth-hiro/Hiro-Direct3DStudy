bc8bb372a5bc6a8a79272a0462cc3828f4131d5f
hth-hiro <hth0114@naver.com>
2025-09-16 18:27:57 +0900

    feat : 카메라 추가

---
+++ Common/Camera.cpp
@@ -0,0 +1,138 @@
+#include "pch.h"
+#include "Camera.h"
+
+Camera::Camera()
+{
+    Reset();
+}
+
+Camera::~Camera()
+{
+	InputSystem::Instance->m_Mouse->SetMode(Mouse::MODE_ABSOLUTE);
+}
+
+void Camera::Reset()
+{
+    m_World = Matrix::Identity;
+    m_Rotation = Vector3(0.0f, 0.0f, 0.0f);
+    m_Position = Vector3(0.0f, 0.0f, -10.0f);
+}
+
+void Camera::Update(float deltaTime)
+{
+    if (m_InputVector.Length() > 0.0f)
+    {
+        m_Position += m_InputVector * m_MoveSpeed * deltaTime;
+        m_InputVector = Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    m_World = Matrix::CreateFromYawPitchRoll(m_Rotation) *
+        Matrix::CreateTranslation(m_Position);
+}
+
+void Camera::GetViewMatrix(Matrix& out)
+{
+    Vector3 eye = m_World.Translation();
+    Vector3 target = m_World.Translation() + GetForward();
+    Vector3 up = m_World.Up();
+    out = XMMatrixLookAtLH(eye, target, up);
+}
+
+Vector3 Camera::GetForward()
+{
+    return -m_World.Forward();
+}
+
+Vector3 Camera::GetRight()
+{
+    return m_World.Right();
+}
+
+void Camera::AddInputVector(const Vector3& input)
+{
+    m_InputVector += input;
+    m_InputVector.Normalize();
+}
+
+void Camera::AddPitch(float value)
+{
+    m_Rotation.x += value;
+
+    // 안정성 고려 크기 제한 (상하)
+    if (m_Rotation.x > XM_PIDIV2)  m_Rotation.x = XM_PIDIV2;
+    if (m_Rotation.x < -XM_PIDIV2) m_Rotation.x = -XM_PIDIV2;
+}
+
+void Camera::AddYaw(float value)
+{
+    m_Rotation.y += value;
+}
+
+void Camera::OnInputProcess(const Keyboard::State& KeyState, const Keyboard::KeyboardStateTracker& KeyTracker, const Mouse::State& MouseState, const Mouse::ButtonStateTracker& MouseTracker)
+{
+    Vector3 forward = GetForward();
+    Vector3 right = GetRight();
+
+	if (KeyTracker.IsKeyPressed(Keyboard::Keys::R))
+	{
+		Reset();
+	}
+
+	if (KeyState.IsKeyDown(Keyboard::Keys::W))
+	{
+		AddInputVector(forward);
+	}
+	else if (KeyState.IsKeyDown(Keyboard::Keys::S))
+	{
+		AddInputVector(-forward);
+	}
+
+	if (KeyState.IsKeyDown(Keyboard::Keys::A))
+	{
+		AddInputVector(-right);
+	}
+	else if (KeyState.IsKeyDown(Keyboard::Keys::D))
+	{
+		AddInputVector(right);
+	}
+
+	if (KeyState.IsKeyDown(Keyboard::Keys::Q))
+	{
+
+		AddInputVector(-m_World.Up());
+	}
+	else if (KeyState.IsKeyDown(Keyboard::Keys::E))
+	{
+		AddInputVector(m_World.Up());
+	}
+
+	if (KeyTracker.IsKeyPressed(Keyboard::Keys::F6))
+	{
+		if (!isFPSMode)
+		{
+			InputSystem::Instance->m_Mouse->SetMode(Mouse::MODE_RELATIVE);
+			isFPSMode = true;
+		}
+		else
+		{
+			InputSystem::Instance->m_Mouse->SetMode(Mouse::MODE_ABSOLUTE);
+			isFPSMode = false;
+		}
+	}
+
+	if (isFPSMode)
+	{
+		Vector3 delta = Vector3(float(MouseState.x), float(MouseState.y), 0.0f) * m_RotationSpeed;
+		
+		// 헷갈리지 않게 주의
+		// Pitch는 X축 기반 회전 -> 카메라 기준 상하
+		// Yaw는 Y축 기반 회전 -> 카메라 기준 좌우  
+		AddPitch(delta.y);
+		AddYaw(delta.x);
+	}
+
+	if (KeyTracker.IsKeyPressed(Keyboard::Keys::Escape))
+	{
+		PostQuitMessage(0);
+	}
+}
---
+++ Common/Camera.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <directxtk/SimpleMath.h>
+#include "InputSystem.h"
+
+using namespace DirectX::SimpleMath;
+
+class Camera : public InputProcesser
+{
+public:
+	Camera();
+	~Camera();
+
+	void Reset();
+	void Update(float deltaTime);
+	void GetViewMatrix(Matrix& out);
+
+	Vector3 GetForward();
+	Vector3 GetRight();
+	Vector3 GetPosition() const { return m_Position; }
+
+	void AddInputVector(const Vector3& input);
+	void AddPitch(float value);
+	void AddYaw(float value);
+
+private:
+	Vector3 m_Rotation;
+	Vector3 m_Position;
+
+	Matrix m_World;
+	Vector3 m_InputVector;
+
+	float m_MoveSpeed = 20.0f;
+	float m_RotationSpeed = 0.004f;	// rad per sec
+
+	bool isFPSMode = false;
+
+	virtual void OnInputProcess(
+		const Keyboard::State& KeyState,
+		const Keyboard::KeyboardStateTracker& KeyTracker,
+		const Mouse::State& MouseState,
+		const Mouse::ButtonStateTracker& MouseTracker
+	) override;
+};
+
