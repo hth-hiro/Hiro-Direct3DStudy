3406a1a4465e1e6a1fd5e7d4fd9911e269c3b6ba
hth-hiro <hth0114@naver.com>
2025-10-20 11:12:20 +0900

    feat : 새 프로젝트 추가

---
+++ 07_BoneAnimation/ImGuiManager.h
@@ -0,0 +1,77 @@
+#pragma once
+#include "../Common/GameApp.h"
+#include "../Common/Camera.h"
+#include "../Common/Transform.h"
+
+#include <imgui.h>
+#include <imgui_impl_win32.h>
+#include <imgui_impl_dx11.h>
+#include <Psapi.h>
+
+#include <directxtk/SimpleMath.h>
+#include <vector>
+
+using namespace DirectX::SimpleMath;
+
+struct Object
+{
+	Object() = default;
+	Object(const XMFLOAT3& pos) : transform(pos), initPos(pos) {}
+
+	Vector3 initPos;
+
+	Transform transform;
+
+	Vector4 ambient = {0.1f, 0.1f, 0.1f, 0.1f};
+	Vector4 diffuse = {1,1,1,1};
+	Vector4 specular = {1,1,1,1};
+
+	float shininess = 1000.f;
+};
+
+class ImGuiManager
+{
+public:
+	ImGuiManager() = default;
+	//ImGuiManager(GameApp* app) : m_app(app) {}
+
+	void Initialize();
+	void BeginFrame(ID3D11Device* _Device, ID3D11DeviceContext* _DeviceContext);
+	void Release() { EndFrame(); }
+	void Render();
+
+	Object object1 = { Vector3(-100,0,0)};
+	Object object2 = { Vector3(0,0,0) };
+	Object object3 = { Vector3(100,0,0) };
+
+	float FOV = 1.0f;
+
+	Vector4 ambientLight = {  };
+	Vector4 diffuseLight = {  };
+	Vector4 specularLight = {  };
+	Vector3 lightDir = {  };
+
+	bool viewChanger = false;
+
+	Vector2 depth = { 1.0f, 10000.0f };
+
+	bool useLighting = true;
+
+private:
+	void EndFrame();
+
+	float nearZ = 1.0f;
+	float farZ = 1000.0f;
+
+	GameApp* m_app;
+
+	void LightReset()
+	{
+		ambientLight = { 0.1f, 0.1f, 0.1f, 0.1f };
+		diffuseLight = { 1.0f, 1.0f, 1.0f, 1.0f };
+		specularLight = { 1.0f, 1.0f, 1.0f, 1.0f };
+		lightDir = { 0.0f, 0.0f, 1.0f };
+	}
+
+	void RenderObjectUI(const char* label, Object& obj);
+};
---
+++ 07_BoneAnimation/ModelLoader.h
@@ -0,0 +1,183 @@
+#ifndef MODEL_LOADER_H
+#define MODEL_LOADER_H
+
+#include <vector>
+#include <d3d11_1.h>
+#include <DirectXMath.h>
+
+#include <assimp\Importer.hpp>
+#include <assimp\scene.h>
+#include <assimp\postprocess.h>
+
+#include "../Common/Transform.h"
+#include "Mesh.h"
+#include "TextureLoader.h"
+
+using namespace DirectX;
+
+// 상수 버퍼를 생성한다. 근데 라이트를 곁들인.
+struct ConstantBuffer
+{
+	Matrix mWorld;
+	Matrix mView;
+	Matrix mProjection;
+
+	Vector4 vLightDir;
+	Vector4 vOutputColor;
+
+	Vector4 vAmbientColor;
+	Vector4 vDiffuseColor;
+	Vector4 vSpecularColor;
+
+	Vector4 vMaterialAmbient;
+	Vector4 vMaterialDiffuse;
+	Vector4 vMaterialSpecular;
+
+	Vector4 cameraPos;
+
+	Vector4 vShininess;
+
+	int UseLighting; // 1 = 빛 계산, 0 = 무시
+	Vector3 padding;
+
+	int hasTexture;
+	
+	int hasNormalMap;
+	int hasSpecularMap;
+	int hasEmissiveMap;
+	
+
+	Vector4 solidColor;
+};
+
+struct Material
+{
+	Vector4 ambient = { 0.1f, 0.1f, 0.1f, 0.1f };
+	Vector4 diffuse = { 0.0f, 0.0f, 0.0f, 0.0f };
+	Vector4 specular = { 0.0f, 0.0f, 0.0f, 0.0f };
+	Vector4 shininess = { 1.0f, 0, 0, 0 };
+};
+
+class Model
+{
+public:
+	std::vector<Mesh> meshes_;
+	std::vector<Texture> textures_loaded_;
+	std::string name;
+	Transform transform;
+	Material material;
+	Transform bone;
+
+	Model() = default;
+
+	void Draw(ID3D11DeviceContext* devcon,
+		ID3D11Buffer* cb,
+		const XMMATRIX& view,
+		const XMMATRIX& proj,
+
+		const Vector4& lightDir,
+		const Vector4& ambient,
+		const Vector4& diffuse,
+		const Vector4& specular,
+
+		const Vector4& shininess,
+
+		const Vector4& cameraPos,
+		const bool& useLighting
+	)
+	{
+		// 특정 부분만 렌더를 할 수 있게 가능하다.
+		ConstantBuffer cbObj;
+
+		for (size_t i = 0; i < meshes_.size(); ++i)
+		{
+			// 상수 버퍼는 매 메시별로 업데이트가 되어야 한다.
+			cbObj.mWorld = XMMatrixTranspose(this->transform.GetMatrix());
+			cbObj.mView = XMMatrixTranspose(view);
+			cbObj.mProjection = XMMatrixTranspose(proj);
+
+			cbObj.vLightDir = lightDir;
+			cbObj.vAmbientColor = ambient;
+			cbObj.vDiffuseColor = diffuse;
+			cbObj.vSpecularColor = specular;
+
+			cbObj.vMaterialAmbient = material.ambient;
+			cbObj.vMaterialDiffuse = material.diffuse;
+			cbObj.vMaterialSpecular = material.specular;
+			cbObj.vShininess = material.shininess;
+
+			cbObj.vOutputColor = XMFLOAT4(1, 1, 1, 1);
+
+			cbObj.cameraPos = cameraPos;
+
+			cbObj.UseLighting = useLighting ? 1 : 0;
+
+			for (auto& tex : meshes_[i].textures_)
+			{
+				if (tex.hasTexture) cbObj.hasTexture = 1;
+				else
+				{
+					cbObj.solidColor = tex.solidColor;
+					cbObj.hasTexture = 0;
+				}
+
+				if (tex.hasNormalMap) cbObj.hasNormalMap = 1;
+				if (tex.hasSpecularMap) cbObj.hasSpecularMap = 1;
+				if (tex.hasEmissiveMap) cbObj.hasEmissiveMap = 1;
+
+			}
+
+			devcon->UpdateSubresource(cb, 0, nullptr, &cbObj, 0, 0);
+
+			devcon->VSSetConstantBuffers(0, 1, &cb);
+			devcon->PSSetConstantBuffers(0, 1, &cb);
+
+			meshes_[i].Draw(devcon);
+		}
+	}
+
+	void Close()
+	{
+		for (auto& mesh : meshes_)
+		{
+			mesh.Close();
+		}
+		meshes_.clear();
+
+		for (auto& tex : textures_loaded_)
+		{
+			tex.Release();
+		}
+		textures_loaded_.clear();
+	}
+};
+
+class ModelLoader
+{
+public:
+	ModelLoader();
+	~ModelLoader();
+
+	//bool Load(ID3D11Device* dev, ID3D11DeviceContext* devcon, std::string filePath);
+
+	bool Load(ID3D11Device* dev, ID3D11DeviceContext* devcon, const std::string& filePath, const std::string& name);
+	//void Draw(ID3D11DeviceContext* devcon, const std::string& name);
+
+	void Close();
+
+	std::vector<Model> models_;
+
+private:
+	ID3D11Device* dev_;
+	ID3D11DeviceContext* devcon_;
+	std::string directory_;
+	
+	HWND hwnd_;
+
+	void processNode(aiNode* node, const aiScene* scene, Model& model);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene, Model& model);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName, const aiScene* scene, Model& model);
+	ID3D11ShaderResourceView* loadEmbeddedTexture(const aiTexture* embeddedTexture);
+};
+
+#endif // !MODEL_LOADER_H
