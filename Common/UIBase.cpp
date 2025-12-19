0d71fd15b43eed41f9fe711ef7e5cdf5e2ffeb8a
hth-hiro <hth0114@naver.com>
2025-12-19 21:54:33 +0900

    feat : UIBase에서 UIButton 등 마우스와 상호작용 가능하게 함수 추가(진행중)

--- Common/UIBase.cpp
+++ Common/UIBase.cpp
@@ -1,19 +1,54 @@
 #include "pch.h"
 #include "UIBase.h"
 
-bool UIBase::HitTest(const POINT& mousePos)
+void UIBase::Update(float dt)
 {
-    return false;
+    for (auto& c : m_children)
+    {
+        if (c && c->IsActive())
+            c->Update(dt);
+    }
 }
 
-void UIBase::SetActive(bool active)
+void UIBase::Shutdown()
 {
-    m_active = active;
+    for (auto& c : m_children)
+    {
+        if (c) c->Shutdown();
+    }
+    m_children.clear();
 }
 
-bool UIBase::IsActive() const
+bool UIBase::HitTest(const POINT& mousePos) const
 {
-    return m_active;
+    if (!m_active || !m_visible || !m_enabled)
+        return false;
+
+    RECT wr = GetWorldBounds();
+    return PtInRect(&wr, mousePos) == TRUE;
+}
+
+void UIBase::SetPosition(int x, int y)
+{
+}
+
+POINT UIBase::GetPosition() const
+{
+    return POINT();
+}
+
+void UIBase::SetSize(int w, int h)
+{
+}
+
+SIZE UIBase::GetSize() const
+{
+    return SIZE();
+}
+
+RECT UIBase::GetWorldBounds() const
+{
+    return RECT();
 }
 
 void UIBase::AddChild(std::unique_ptr<UIBase> child)
--- Common/UIBase.h
+++ Common/UIBase.h
@@ -22,26 +22,75 @@ using namespace DirectX;
 class UIBase
 {
 public:
+    enum class State
+    {
+        Normal, Hovered, Pressed, Disabled
+    };
+
+public:
     virtual ~UIBase() = default;
 
     virtual bool Initialize() { return true; }
-    virtual void Update(float dt) {}
+    virtual void Update(float dt);
     virtual void Render() = 0;
-    virtual void Shutdown() {}
+    virtual void Shutdown();
 
-    virtual bool HitTest(const POINT& mousePos);
+    virtual bool HitTest(const POINT& mousePos) const;
 
-    void SetActive(bool active);
-    bool IsActive() const;
+    void SetActive(bool active) { m_active = active; }
+    bool IsActive() const { return m_active; }
+
+    void SetVisible(bool visible) { m_visible = visible; }
+    bool IsVisible() const { return m_visible; }
+
+    void SetEnabled(bool enabled)
+    {
+        m_enabled = enabled;
+        m_state = m_enabled ? State::Normal : State::Disabled;
+    }
+    bool IsEnabled() const { return m_enabled; }
+
+    State GetState() const { return m_state; }
+    void SetState(State s) { m_state = s; }
 
     int GetZOrder() const { return m_zOrder; }
     void SetZOrder(int z) { m_zOrder = z; }
 
+    void SetBounds(const RECT& r) { m_bounds = r; }
+    const RECT& GetBounds() const { return m_bounds; }
+
+    void SetPosition(int x, int y);
+    POINT GetPosition() const;
+
+    void SetSize(int w, int h);
+    SIZE GetSize() const;
+
+    RECT GetWorldBounds() const;
+
+    // Tree구조를 만든다.
     void AddChild(std::unique_ptr<UIBase> child);
+    UIBase* GetParent() const { return m_parent; }
+
+    // 버튼과 상호작용하는 함수, UIManager가 호출
+    virtual void OnMouseEnter() {}
+    virtual void OnMouseLeve() {}
+    virtual void OnMouseMove(const POINT&) {}
+    virtual void OnMouseDown(const POINT&) {}
+    virtual void OnMouseUp(const POINT&) {}
+    virtual void OnClick() {}
 
 protected:
-    RECT m_bounds;
-    bool m_active = true;
+    RECT m_bounds = {0, 0, 0, 0};
+
+    UIBase* m_parent = nullptr;
+    std::vector<std::unique_ptr<UIBase>> m_children;
+    
     int m_zOrder = 0;
+
+    bool m_active = true;
+    bool m_visible = true;
+    bool m_enabled = true;
+
+    State m_state = State::Normal;
 };
 
